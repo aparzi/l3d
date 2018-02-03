2b022b0c7ff1d6d1c2acd28d3f0cd513fbb073d2
angelo <angeloparziale94@gmail.com>
2018-02-03 12:12:11 +0100

    inserito il primo prodotto del catalogo, con redirect al pdf

---
+++ catalogo.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'partials/head.html' ?>
+<body>
+<!--loader-->
+<div id="preloader">
+    <div class="sk-circle">
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+    </div>
+</div>
+
+<!--loader-->
+<!-- HEADER -->
+<!--Start header area-->
+<header class="header__block">
+    <div class="top-part__block ptb-15">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7">
+                    <p>
+                        Welcome to our corporate buisness
+                    </p>
+                </div>
+                <div class="col-sm-5">
+                    <div class="social-link__block text-right">
+                        <a href="services.html#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="services.html#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="services.html#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="services.html#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="middel-part__block ptb-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <a href="index1.html"> <img src="assets/images/logo.png" alt="Logo"> </a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="top-info__block text-right">
+                        <ul>
+                            <li>
+                                <i class="fa fa-map-marker"></i>
+                                <p>
+                                    13005 Greenvile Avenue <span> California, TX 70240</span>
+                                </p>
+                            </li>
+                            <li>
+                                <i class="fa fa-phone"></i>
+                                <p>
+                                    Call Us <span> +56 (0) 012 345 6789</span>
+                                </p>
+                            </li>
+                            <li>
+                                <i class="fa fa-clock-o"></i>
+                                <p>
+                                    Opening Time <span> Mon -Sat: 7:00 - 17:00</span>
+                                </p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'partials/navbar.html' ?>
+</header>
+<!--End mainmenu area-->
+<!-- END HEADER -->
+<!-- Intro Section -->
+<section class="inner-intro bg-img light-color overlay-before parallax-background">
+    <div class="container">
+        <div class="row title">
+            <h1>Services</h1>
+            <div class="page-breadcrumb">
+                <a href="index.php" style="cursor: pointer;">Home</a> / <span>Catalogo</span>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Intro Section -->
+<!-- Service Section -->
+<div id="services-section" class="padding ptb-xs-40">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-sm-12">
+                <ul class="project-col-3 isotope" id="isotope">
+                    <li>
+                        <div class="col-md-4 full-wid mb-30">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <img style="height: 225px;" class="img-responsive" src="http://castellochannel.it/wp-content/uploads/2017/02/lampade-led-fari-illuminazione-.jpg" alt="Photo">
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3>Apollo 12L 700</h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a target="_blank" href="http://www.l3dtech.it/schede_tecniche/specifiche/Apollo12L_700.pdf" class="read__more"><i class="fa fa-arrow-circle-o-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="col-md-4 full-wid mb-30">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <a href="services.html#"><img class="img-responsive" src="assets/images/services/servies-2.jpg" alt="Photo"></a>
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3><a href="services.html#">General Contracting</a></h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a href="services.html#" class="read__more"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="col-md-4 full-wid mb-30">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <a href="services.html#"><img class="img-responsive" src="assets/images/services/servies-4.jpg" alt="Photo"></a>
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3><a href="services.html#">House Renovation</a></h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a href="services.html#" class="read__more"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="col-md-4 full-wid">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <a href="services.html#"><img class="img-responsive" src="assets/images/services/servies-0.jpg" alt="Photo"></a>
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3><a href="services.html#">Green Building</a></h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a href="services.html#" class="read__more"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="col-md-4 full-wid">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <a href="services.html#"><img class="img-responsive" src="assets/images/services/servies-5.jpg" alt="Photo"></a>
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3><a href="services.html#">Laminate Flooring</a></h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a href="services.html#" class="read__more"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="col-md-4 full-wid">
+                            <div class="carousel-item">
+                                <div class="about-block img-scale  mb-xs-40 clearfix border__b">
+                                    <figure>
+                                        <a href="services.html#"><img class="img-responsive" src="assets/images/services/servies-6.jpg" alt="Photo"></a>
+                                    </figure>
+                                    <div class="text-box mt-25">
+                                        <div class="box-title mb-15">
+                                            <h3><a href="services.html#">construction Consultant</a></h3>
+                                        </div>
+                                        <div class="text-content">
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing nulla quis fermentum hendrerit, nisi diam viverra feugiat viverra diam.felis tortor
+                                            </p>
+                                            <a href="services.html#" class="read__more"><i class="ion-ios-arrow-thin-right"></i></a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Service Section end -->
+
+<!-- FOOTER -->
+<?php include 'partials/footer.html' ?>
+<!-- END FOOTER -->
+</div>
+<!-- Site Wraper End -->
+<script src="assets/js/jquery-1.12.4.min.js" type="text/javascript"></script>
+<!-- Easing Effect Js -->
+<script src="assets/js/plugin/jquery.easing.js" type="text/javascript"></script>
+<!-- bootstrap Js -->
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- carousel Js -->
+<script src="assets/js/plugin/owl.carousel.js" type="text/javascript"></script>
+<!-- imagesloaded Js -->
+<script src="assets/js/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+<!-- masonry,isotope Effect Js -->
+<script src="assets/js/isotope.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/js/masonry.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/js/jquery.appear.js" type="text/javascript"></script>
+<!-- parallax Js -->
+<script src="assets/js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+<!-- custom Js -->
+<script src="assets/js/custom.js" type="text/javascript"></script>
+</body>
+</html>
