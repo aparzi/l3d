ad274e0c4e6ab330dfecfad87234ff846d8dade2
angelo <angeloparziale94@gmail.com>
2018-02-03 11:26:22 +0100

    quando il server restuisce un 404 viene visualizzata una pagina custom

---
+++ 404.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'partials/head.html' ?>
+<body>
+<!--loader-->
+<div id="preloader">
+    <div class="sk-circle">
+        <div class="sk-circle1 sk-child"></div>
+        <div class="sk-circle2 sk-child"></div>
+        <div class="sk-circle3 sk-child"></div>
+        <div class="sk-circle4 sk-child"></div>
+        <div class="sk-circle5 sk-child"></div>
+        <div class="sk-circle6 sk-child"></div>
+        <div class="sk-circle7 sk-child"></div>
+        <div class="sk-circle8 sk-child"></div>
+        <div class="sk-circle9 sk-child"></div>
+        <div class="sk-circle10 sk-child"></div>
+        <div class="sk-circle11 sk-child"></div>
+        <div class="sk-circle12 sk-child"></div>
+    </div>
+</div>
+
+<!--loader-->
+<!-- HEADER -->
+<!--Start header area-->
+<header class="header__block">
+    <div class="top-part__block ptb-15">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-7">
+                    <p>Welcome to our corporate buisness</p>
+                </div>
+                <div class="col-sm-5">
+                    <div class="social-link__block text-right">
+                        <a href="partials/404.html#" class="facebook"><i class="fa fa-facebook"></i></a>
+                        <a href="partials/404.html#" class="twitter"><i class="fa fa-twitter"></i></a>
+                        <a href="partials/404.html#" class="google-plus"><i class="fa fa-google-plus"></i></a>
+                        <a href="partials/404.html#" class="linkedin"><i class="fa fa-linkedin"></i></a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="middel-part__block ptb-20">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="logo">
+                        <a href="index1.html">
+                            <img src="assets/images/logo.png" alt="Logo">
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="top-info__block text-right">
+                        <ul>
+                            <li>
+                                <i class="fa fa-map-marker"></i>
+                                <p> 13005 Greenvile Avenue <span> California, TX 70240</span></p>
+                            </li>
+                            <li>
+                                <i class="fa fa-phone"></i>
+                                <p>Call Us <span> +56 (0) 012 345 6789</span></p>
+                            </li>
+                            <li>
+                                <i class="fa fa-clock-o"></i>
+                                <p>Opening Time <span> Mon -Sat: 7:00 - 17:00</span></p>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <?php include 'partials/navbar.html' ?>
+</header>
+<!--End mainmenu area-->
+<!-- END HEADER -->
+<!-- CONTENT -->
+<div class="page-404 ptb-80 ptb-xs-60">
+    <div class="container">
+        <!--Footer Info -->
+        <div class="row text-center">
+            <div class="col-sm-6 col-sm-offset-3"> <strong class="big-font">4<i class="ion-android-happy"></i>4</strong>
+                <h2>Page Not Found..</h2>
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae odit iste exerc ssumenda voluptas quidem sit maiores odio velit voluptate. </p>
+                <a href="index.php" class="btn-text mt-15">Back to home page</a> </div>
+        </div>
+    </div>
+</div>
+<!-- FOOTER -->
+<?php include 'partials/footer.html' ?>
+<!-- END FOOTER -->
+</div>
+<!-- Site Wraper End -->
+<script src="assets/js/jquery.min.js" type="text/javascript"></script>
+<!-- Easing Effect Js -->
+<script src="assets/js/plugin/jquery.easing.js" type="text/javascript"></script>
+<!-- bootstrap Js -->
+<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
+<!-- imagesloaded Js -->
+<script src="assets/js/imagesloaded.pkgd.min.js" type="text/javascript"></script>
+<!-- masonry,isotope Effect Js -->
+<script src="assets/js/isotope.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/js/masonry.pkgd.min.js" type="text/javascript"></script>
+<script src="assets/js/jquery.appear.js" type="text/javascript"></script>
+<!-- parallax Js -->
+<script src="assets/js/jquery.parallax-1.1.3.js" type="text/javascript"></script>
+<!-- carousel Js -->
+<script src="assets/js/plugin/owl.carousel.js" type="text/javascript"></script>
+<!-- revolution Js -->
+<script type="text/javascript" src="assets/js/jquery.themepunch.tools.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
+<script type="text/javascript" src="assets/extensions/revolution.extension.slideanims.min.js"></script>
+<script type="text/javascript" src="assets/extensions/revolution.extension.layeranimation.min.js"></script>
+<script type="text/javascript" src="assets/extensions/revolution.extension.navigation.min.js"></script>
+<script type="text/javascript" src="assets/extensions/revolution.extension.parallax.min.js"></script>
+<script type="text/javascript" src="assets/js/jquery.revolution.js"></script>
+<!-- Map api Js -->
+<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&.js"></script>
+<!-- custom Js -->
+<script src="assets/js/custom.js" type="text/javascript"></script>
+</body>
+</html>
